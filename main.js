419d6bdd7c213f113f3b0a0c56220619960c5ad3
Josh <joshru@uw.edu>
2016-01-12 21:43:15 -0800

    Sprite shows up and animates now.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var AM = new AssetManager();
-
-function Animation(spriteSheet, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
     this.spriteSheet = spriteSheet;
+    this.startX = startX;
+    this.startY = startY;
     this.frameWidth = frameWidth;
     this.frameDuration = frameDuration;
     this.frameHeight = frameHeight;
@@ -12,73 +12,172 @@ function Animation(spriteSheet, frameWidth, frameHeight, frameDuration, frames, 
     this.reverse = reverse;
 }
 
-Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+    var scaleBy = scaleBy || 1;
     this.elapsedTime += tick;
-    if (this.isDone()) {
-        if (this.loop) this.elapsedTime = 0;
+    if (this.loop) {
+        if (this.isDone()) {
+            this.elapsedTime = 0;
+        }
+    } else if (this.isDone()) {
+        return;
     }
-    var frame = this.currentFrame();
-    var xindex = 0;
-    var yindex = 0;
-    if (frame > 13) {
-        frame = 26 - frame;
+    var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
+    var vindex = 0;
+    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
+        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
+        vindex++;
     }
-    xindex = frame % 5;
-    yindex = Math.floor(frame / 5);
+    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
+        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
+        vindex++;
+    }
 
-    console.log(frame + " " + xindex + " " + yindex);
-
+    var locX = x;
+    var locY = y;
+    var offset = vindex === 0 ? this.startX : 0;
     ctx.drawImage(this.spriteSheet,
-                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
-                 this.frameWidth, this.frameHeight,
-                 x, y,
-                 this.frameWidth,
-                 this.frameHeight);
-}
+                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+                  this.frameWidth, this.frameHeight,
+                  locX, locY,
+                  this.frameWidth * scaleBy,
+                  this.frameHeight * scaleBy);
+};
 
 Animation.prototype.currentFrame = function () {
     return Math.floor(this.elapsedTime / this.frameDuration);
-}
+};
 
 Animation.prototype.isDone = function () {
     return (this.elapsedTime >= this.totalTime);
+};
+
+function Background(game) {
+    Entity.call(this, game, 0, 400);
+    this.radius = 200;
 }
 
-function MushroomDude(game, spritesheet) {
-    this.animation = new Animation(spritesheet, 189, 230, 0.05, 26, true, false);
-    this.x = 0;
-    this.y = 0;
-    this.game = game;
-    this.ctx = game.ctx;
+Background.prototype = new Entity();
+Background.prototype.constructor = Background;
+
+Background.prototype.update = function () {
+};
+
+Background.prototype.draw = function (ctx) {
+    ctx.fillStyle = "#173A15";
+    ctx.fillRect(0,500,800,300);
+    Entity.prototype.draw.call(this);
+};
+
+function Unicorn(game) {
+    this.animation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 0, 0, 206, 110, 0.02, 30, true, true);
+    this.jumpAnimation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 618, 334, 174, 138, 0.02, 40, false, true);
+    this.jumping = false;
+    this.radius = 100;
+    this.ground = 300;
+    Entity.call(this, game, 0, 300);
 }
 
-MushroomDude.prototype.draw = function () {
-//    console.log("drawing");
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+Unicorn.prototype = new Entity();
+Unicorn.prototype.constructor = Unicorn;
+
+Unicorn.prototype.update = function () {
+    if (this.game.space) this.jumping = true;
+    if (this.jumping) {
+        if (this.jumpAnimation.isDone()) {
+            this.jumpAnimation.elapsedTime = 0;
+            this.jumping = false;
+        }
+        var jumpDistance = this.jumpAnimation.elapsedTime / this.jumpAnimation.totalTime;
+        var totalHeight = 200;
+
+        if (jumpDistance > 0.5)
+            jumpDistance = 1 - jumpDistance;
+
+        //var height = jumpDistance * 2 * totalHeight;
+        var height = totalHeight*(-4 * (jumpDistance * jumpDistance - jumpDistance));
+        this.y = this.ground - height;
+    }
+    Entity.prototype.update.call(this);
+};
+
+Unicorn.prototype.draw = function (ctx) {
+    if (this.jumping) {
+        this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x + 17, this.y - 34);
+    }
+    else {
+        this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }
+    Entity.prototype.draw.call(this);
+};
+
+function Snake(game) {
+    this.animation = new Animation(ASSET_MANAGER.getAsset("./img/snake.png"), 10, 143, 35.5, 49, 0.2, 6, true, false);
+    this.boxAnimation = new Animation(ASSET_MANAGER.getAsset("./img/snake.png"), 65, 1763, 43, 67, 0.2, 8, true, false);
+    this.box = false;
+    this.radius = 100;
+    this.ground = 400;
+    Entity.call(this, game, 0, 450);
 }
+Snake.prototype = new Entity();
+Snake.prototype.constructor = Snake;
 
-MushroomDude.prototype.update = function() {
-    this.x += 2;
-}
+Snake.prototype.update = function() {
+    //this.x += 1;
 
+    if (this.game.space) {
+        this.box ^= true;
+        console.log(this.box);
+    }
+    if (this.box) {
+        if (this.boxAnimation.isDone()) {
+            this.boxAnimation.elapsedTime = 0;
+            this.box ^= true;
+        }
+        Entity.prototype.draw.call(this);
+    }
+    //if (this.game.space) this.jumping = true;
+    //if (this.jumping) {
+    //    if (this.jumpAnimation.isDone()) {
+    //        this.jumpAnimation.elapsedTime = 0;
+    //        this.jumping = false;
+    //    }
+    //    var jumpDistance = this.jumpAnimation.elapsedTime / this.jumpAnimation.totalTime;
+    //    var totalHeight = 200;
+    //
+    //    if (jumpDistance > 0.5) {
+    //        jumpDistance = 1 - jumpDistance;
+    //    }
+    //    var height = totalHeight * (-4 * (jumpDistance * jumpDistance - jumpDistance));
+    //    this.y = this.ground - height;
+    //}
+    //Entity.prototype.draw.call(this);
+};
 
-AM.queueDownload("./img/RobotUnicorn.png");
-AM.queueDownload("./img/guy.png");
-AM.queueDownload("./img/mushroomdude.png");
-AM.queueDownload("./img/runningcat.png");
-AM.queueDownload("./img/notthere.png");
+Snake.prototype.draw = function(ctx) {
+    this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    Entity.prototype.draw.call(this);
+};
 
-AM.downloadAll(function () {
-    var canvas = document.getElementById("gameWorld");
-    var ctx = canvas.getContext("2d");
+// the "main" code begins here
 
-//    var img = AM.getAsset("./img/mushroomdude.png");
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./img/snake.png");
+
+ASSET_MANAGER.downloadAll(function () {
+    console.log("starting up da sheild");
+    var canvas = document.getElementById('gameWorld');
+    var ctx = canvas.getContext('2d');
 
     var gameEngine = new GameEngine();
+    var bg = new Background(gameEngine);
+    //var unicorn = new Unicorn(gameEngine);
+    var snake = new Snake(gameEngine);
+    gameEngine.addEntity(bg);
+    //gameEngine.addEntity(unicorn);
+    gameEngine.addEntity(snake);
+
     gameEngine.init(ctx);
     gameEngine.start();
-
-    gameEngine.addEntity(new MushroomDude(gameEngine, AM.getAsset("./img/mushroomdude.png")));
-
-    console.log("All Done!");
 });
