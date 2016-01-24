b970e5e990c1f118c59417965ab3444f95eceb81
Josh <joshru@uw.edu>
2016-01-23 20:20:20 -0800

    SO close

--- main.js
+++ main.js
@@ -72,47 +72,6 @@ Background.prototype.draw = function (ctx) {
     Entity.prototype.draw.call(this);
 };
 
-function Unicorn(game) {
-    this.animation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"),     0,   0,   206, 110, 0.02, 30, true, false);
-    this.jumpAnimation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 618, 334, 174, 138, 0.02, 40, false, true);
-    this.jumping = false;
-    this.radius = 100;
-    this.ground = 300;
-    Entity.call(this, game, 0, 300);
-}
-
-Unicorn.prototype = new Entity();
-Unicorn.prototype.constructor = Unicorn;
-
-Unicorn.prototype.update = function () {
-    if (this.game.space) this.jumping = true;
-    if (this.jumping) {
-        if (this.jumpAnimation.isDone()) {
-            this.jumpAnimation.elapsedTime = 0;
-            this.jumping = false;
-        }
-        var jumpDistance = this.jumpAnimation.elapsedTime / this.jumpAnimation.totalTime;
-        var totalHeight = 200;
-
-        if (jumpDistance > 0.5)
-            jumpDistance = 1 - jumpDistance;
-
-        //var height = jumpDistance * 2 * totalHeight;
-        var height = totalHeight*(-4 * (jumpDistance * jumpDistance - jumpDistance));
-        this.y = this.ground - height;
-    }
-    Entity.prototype.update.call(this);
-};
-
-Unicorn.prototype.draw = function (ctx) {
-    if (this.jumping) {
-        this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x + 17, this.y - 34);
-    }
-    else {
-        this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
-    }
-    Entity.prototype.draw.call(this);
-};
 function Snake(game) {
     this.game = game;
     //store all our animations in Snake so we don't have to make new ones all the time
